1eed66fc996279e39559616de120258c5424454b
John Karr <jjkkarr1@live.com>
2016-03-24 14:38:56 -0400

    Changed PlayerHealth to be test friendly

---
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/Editor/PlayerHealthTests.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using PH;
+    public class PlayerHealthTests
+    {
+
+    [Test]
+    public void PlayerLossAllHealth()
+    {
+        
+        
+    }
+
+    }
--- SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/PlayerHealth.cs
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/PlayerHealth.cs
@@ -1,76 +1,81 @@
-﻿using UnityEngine;
+﻿namespace PH
+{
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerHealth : MonoBehaviour
-{
-    public int startingHealth = 100;                            // The amount of health the player starts the game with.
-    public int currentHealth;                                   // The current health the player has.
-    public Slider healthSlider;                                 // Reference to the UI's health bar.
-    public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
-  
-    public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
 
-    bool isDead;                                                // Whether the player is dead.
-    bool damaged;                                               // True when the player gets damaged.
-
-
-    void Awake()
+    public class PlayerHealth : MonoBehaviour
     {
-       
-        currentHealth = startingHealth;
-    }
+        public int startingHealth = 100;                            // The amount of health the player starts the game with.
+        public int currentHealth;                                   // The current health the player has.
+        public Slider healthSlider;                                 // Reference to the UI's health bar.
+        public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+
+        public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
+        public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+
+        bool isDead;                                                // Whether the player is dead.
+        bool damaged;                                               // True when the player gets damaged.
 
 
-    void Update()
-    {
-        // If the player has just been damaged...
-        if (damaged)
+        void Awake()
         {
-            // ... set the colour of the damageImage to the flash colour.
-            damageImage.color = flashColour;
-        }
-        // Otherwise...
-        else
-        {
-            // ... transition the colour back to clear.
-            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+
+            currentHealth = startingHealth;
         }
 
-        // Reset the damaged flag.
-        damaged = false;
-    }
 
-
-    public void TakeDamage(int amount)
-    {
-        // Set the damaged flag so the screen will flash.
-        damaged = true;
-
-        // Reduce the current health by the damage amount.
-        currentHealth -= amount;
-
-        // Set the health bar's value to the current health.
-        healthSlider.value = currentHealth;
-      
-
-        // If the player has lost all it's health and the death flag hasn't been set yet...
-        if (currentHealth <= 0 && !isDead)
+        void Update()
         {
-            // ... it should die.
-            Death();
+            // If the player has just been damaged...
+            if (damaged)
+            {
+                // ... set the colour of the damageImage to the flash colour.
+                damageImage.color = flashColour;
+            }
+            // Otherwise...
+            else
+            {
+                // ... transition the colour back to clear.
+                damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            }
+
+            // Reset the damaged flag.
+            damaged = false;
         }
-    }
 
 
-    void Death()
-    {
-        // Set the death flag so this function won't be called again.
-        isDead = true;
+        public  float TakeDamage(int amount)
+        {
+            // Set the damaged flag so the screen will flash.
+            damaged = true;
 
-       
+            // Reduce the current health by the damage amount.
+            currentHealth -= amount;
 
-      
-    }
-}
+            // Set the health bar's value to the current health.
+            healthSlider.value = currentHealth;
+
+
+            // If the player has lost all it's health and the death flag hasn't been set yet...
+            if (currentHealth <= 0 && !isDead)
+            {
+                // ... it should die.
+                Death();
+            }
+            return healthSlider.value;//put here for testing purposes
+        }
+
+
+        void Death()
+        {
+            // Set the death flag so this function won't be called again.
+            isDead = true;
+
+
+
+
+        }
+    }//end PlayerHealth
+}//end namespace
