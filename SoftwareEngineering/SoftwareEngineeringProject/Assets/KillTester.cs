df22bddc9150562708b0e8835aaac8e4c40cb7e9
John Karr <jjkkarr1@live.com>
2016-03-25 21:23:00 -0400

    Added EnemyAttack
    I added an EnemyAttack Script along with a sphere dubbed ThunderDOME to
    test playerhealth. Tests documented in playerhealth test documentaion
    word file.

---
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/KillTester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using PH;
+public class KillTester : MonoBehaviour {
+
+    PlayerHealth test;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void onTriggerEnter()
+    {
+        test.TakeDamage(50);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
