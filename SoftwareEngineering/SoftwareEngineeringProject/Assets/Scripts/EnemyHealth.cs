72157c5f4285474cb77855266363fb108668911d
Andrew Klem <amklem@gmail.com>
2016-04-02 00:21:21 -0400

    Add rough draft of EnemyHealth script

---
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    /*
+    DECLARE VARIABLES
+    */
+
+    //enemy health
+    public int startingHealth = 100;            
+    public int currentHealth;
+    
+    //enemy will sink through floor upon death   
+    public float deathSinkSpeed = 2.5f;
+
+    //points (for game type 2)
+    //public int scoreValue = 10;
+
+    //Audio clip of death, if needed
+    //public AudioClip deathClip;
+
+    //references to various Unity components
+    Animator anim;
+    AudioSource enemyAudio;
+    //ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+
+    //death state
+    bool isDead;
+    bool isSinking;
+
+    void Awake()
+    {
+        // Set up the references
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        //hitParticles = GetComponentInChildren<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        // Set the current health when the enemy first spawns
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        // If the enemy should be sinking, move enemy down by sink speed per second
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * deathSinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        // If the enemy is dead, exit function
+        if (isDead)
+            return;
+
+        // Play the hurt sound effect.
+        //enemyAudio.Play();
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+
+        // Set the position of the particle system to where the hit was sustained.
+        //hitParticles.transform.position = hitPoint;
+
+        // And play the particles.
+        //hitParticles.Play();
+
+        // Enemy dies if health goes to 0
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        // The enemy is dead
+        isDead = true;
+
+        // Turn the collider into a trigger so shots can pass through it
+        capsuleCollider.isTrigger = true;
+
+        // Tell the animator that the enemy is dead
+        anim.SetTrigger("Dead");
+
+        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing)
+        //enemyAudio.clip = deathClip;
+        //enemyAudio.Play();
+    }
+
+
+    public void StartSinking()
+    {
+        // Find and disable the Nav Mesh Agent
+        GetComponent<NavMeshAgent>().enabled = false;
+
+        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy)
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        // The enemy should now sink
+        isSinking = true;
+
+        // Increase the score by the enemy's score value
+       // ScoreManager.score += scoreValue;
+
+        // After 2 seconds destory the enemy.
+        Destroy(gameObject, 2f);
+    }
+}
