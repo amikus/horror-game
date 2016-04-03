f59d80030d2b7662d02775962578c0c9a2740a67
drbarber <sgosman506@gmail.com>
2016-04-03 15:04:46 -0400

    Main Menu

---
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/menuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText; //play button
+    public Button exitText; // quit button
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+
+	}
+	
+    public void exitPressed()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void noPressed()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void startLevel()
+    {
+        SceneManager.LoadScene("Sam's Scene");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
