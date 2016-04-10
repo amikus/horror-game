366cf67d62b6e406fd32f7800aad106e32abbcd7
drbarber <sgosman506@gmail.com>
2016-04-10 13:21:04 -0400

    can win
    *Player can now win the game
    *added Key asset
    *added 3D grass to the ground
    *Changed tree colliders to a mesh (this change is not in effect yet! if
    i changed it would break anybody elses main scene)

--- SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/PlayerController.js
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/PlayerController.js
@@ -1,5 +1,6 @@
 ﻿// the PickupController component of the 'PickupSpawnPoints' GameObject
 private var pickupController:PickupController;
+private var keyCollected = false;
  
 function Awake()
 {
@@ -16,7 +17,13 @@ function OnControllerColliderHit (hit:ControllerColliderHit)
         {
             // call the Collected(...) function of the PickupController Component (script) and 
             // pass the pickup we hit as the parameter for the function
+            keyCollected = true;
             pickupController.Collected(hit.gameObject);
+        }
+        if(hit.gameObject.tag == "Door" && keyCollected == true)
+        {
+            Destroy(hit.gameObject);
+            Application.LoadLevel ("Victory");
         }
     }
 
