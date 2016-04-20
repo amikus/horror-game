5f319511ad48a1f322d4337301b25d176f2f13c8
Andrew Klem <amklem@gmail.com>
2016-04-19 22:23:21 -0400

    Converted PickupController and PlayerController to C#

---
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/PickupController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupController : MonoBehaviour {
+
+
+    //the pickup prefab, assigned via the Inspector
+    public GameObject pickupPrefab;
+
+    //the number of pickups to have around the level at any one time
+    public int numberOfPickups = 2;
+
+    //the array of spawnpoints that our pickup will be spawned at
+    private GameObject[] spawnPointList;
+
+    //array of which spawn points are currently available for spawning at
+    private ArrayList spawnIndexAvailableList = new ArrayList();
+
+    //variable to hold total number of spawn points, saves having to recalculate
+    private int numberOfSpawnPoints;
+
+    void Awake ()
+    {
+        //retrieve GameObjects tagged as 'SpawnPoint' within the 'PickupSpawnPoints' GameObject which this script is a Component of
+        spawnPointList = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+        //retrieve number of spawn points
+        numberOfSpawnPoints = spawnPointList.Length;
+
+        //make sure number of pickups doesn't exceed number of spawn points
+        if (numberOfPickups > numberOfSpawnPoints) {
+            numberOfPickups = numberOfSpawnPoints;
+        }
+
+        // make all spawn points available by setting each index to true
+        for (int i = 0; i < numberOfSpawnPoints; i++)
+        {
+            spawnIndexAvailableList[i] = true;
+        }
+
+        // spawn X amount of pickups according to numberOfPickups
+        for (int j = 0; j < numberOfPickups; j++) 
+        {
+            SpawnPickup();
+        }
+    }
+
+    void SpawnPickup()
+    {
+        // generate a random integer to use as the index to select a spawn point from the list
+        int randomSpawnIndex = Random.Range(0, numberOfSpawnPoints);
+
+        // while the selected spawn index is unavailable regenerate another one
+        while (spawnIndexAvailableList[randomSpawnIndex] == null)
+        {
+            randomSpawnIndex = Random.Range(0, numberOfSpawnPoints);
+        }
+
+        // retrieve the position and rotation of the pickups spawn point
+        Vector3 spawnedPickupPosition = spawnPointList[randomSpawnIndex].transform.position;
+        Quaternion spawnedPickupRotation = spawnPointList[randomSpawnIndex].transform.rotation;
+
+        // instantiate (create) the pickup prefab with the above position and rotation
+        GameObject spawnedPickup = Instantiate(pickupPrefab, spawnedPickupPosition, spawnedPickupRotation) as GameObject;
+
+        // set the spawned pickup as a child of the 'PickupSpawnPoints' gameobject that this script is a Component of
+        // this is so we can use SendMessageUpwards within scripts attached to the pickupPrefab to call functions within this script
+        spawnedPickup.transform.parent = spawnPointList[randomSpawnIndex].transform;
+
+        // set the name of the pickup as its index
+        spawnedPickup.name = randomSpawnIndex.ToString();
+
+        // make the spawn index unavailable to prevent another pickup being spawned in this position
+        spawnIndexAvailableList[randomSpawnIndex] = false;
+    }
+
+    void Collected(GameObject pickupCollected)
+    {
+        // retrieve name of the collected pickup and cast to int
+        int index = int.Parse(pickupCollected.name);
+
+        // pickup has been destroyed so make the spawn index available again
+        spawnIndexAvailableList[index] = true;
+
+        // destroy the pickup
+        Destroy(pickupCollected);
+
+        //respawn new pickup
+        //SpawnPickup();
+
+        //We can possibly add more functionality to this function, such as having it respawn items after a set amount of time
+
+    }
+}
---
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private PickupController pickupController;
+    private bool keyCollected = false;
+
+    void Awake ()
+    {
+        //retrieve the PickupSpawnPoints GameObject
+        GameObject pickupSpawnPoints = GameObject.Find("KeySpawnPoints");
+
+        //and then retrieve the PickupController Component of the above PickupSpawnPoints GameObject 
+        pickupController = pickupSpawnPoints.GetComponent("PickupController");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Pickup")
+        {
+            //call the Collected(...) function of the PickupController Component (script) and
+            //pass the pickup we hit as the parameter for the function
+            keyCollected = true;
+            pickupController.Collected(hit.gameObject);
+
+        }
+
+        if (hit.gameObject.tag == "Door" && keyCollected == true)
+        {
+            Destroy(hit.gameObject);
+            Application.LoadLevel("Victory");
+        }
+    }
+}
+
+
