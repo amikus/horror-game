da76cc25c1bce374bf00b31c28602791f9427d8f
John Karr <jjkkarr1@live.com>
2016-04-20 23:01:26 -0400

    Added Weapon Pickup Script
    Added weapon pickup script

---
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/WeaponPickup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponPickup : MonoBehaviour
+{
+
+    public GameObject AK;
+    public GameObject DummyAK;
+    public Collider AkColl;
+    // Use this for initialization
+    void Start()
+    {
+        AkColl = GetComponent<Collider>();
+        DummyAK = GetComponent<GameObject>();
+        AK.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "AK")
+        {
+
+            Destroy(other.gameObject);
+            AK.SetActive(true);
+        }
+    }
+
+
+}
