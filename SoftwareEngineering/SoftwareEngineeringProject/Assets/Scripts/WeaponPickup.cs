5f1559d01011f54d76023daeca063bb05a0f7fd3
John Karr <jjkkarr1@live.com>
2016-04-21 13:33:52 -0400

    WeaponPickup Script edited
    Added Weapon Drop // No weapon spawns yet when you drop gun
    Added Weapon Switch// Works with Two weapons equipped

--- SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/WeaponPickup.cs
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Scripts/WeaponPickup.cs
@@ -5,14 +5,16 @@ public class WeaponPickup : MonoBehaviour
 {
 
     public GameObject AK;
+    public GameObject gun2;
     public GameObject DummyAK;
-    public Collider AkColl;
+    float x;
+    float z;
+    public  int gunCount;
     // Use this for initialization
     void Start()
     {
-        AkColl = GetComponent<Collider>();
-        DummyAK = GetComponent<GameObject>();
         AK.SetActive(false);
+        gun2.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other)
@@ -20,11 +22,82 @@ public class WeaponPickup : MonoBehaviour
 
         if (other.tag == "AK")
         {
-
+            gunCount++;
             Destroy(other.gameObject);
-            AK.SetActive(true);
-        }
+            if (gunCount == 2)
+            {
+                gun2.SetActive(false);
+                AK.SetActive(true);
+
+            }
+            else
+            {
+                AK.SetActive(true);
+            }
+
+        }//end if
+
+        if(other.tag == "AKBlank")
+        {
+            gunCount++;
+            Destroy(other.gameObject);
+            if (gunCount == 2)
+            {
+                AK.SetActive(false);
+                gun2.SetActive(true);
+            }
+            else
+            {
+                gun2.SetActive(true);
+            }
+            
+        }//end if
     }
 
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))//Weapon Drop
+        {
+            if(AK.activeSelf)
+            {   gunCount--;
+               
+                AK.SetActive(false);
+              if(gunCount == 1)
+                {
+                    gun2.SetActive(true);
+                }
+                
+            }
 
+            if (gun2.activeSelf)
+            {
+                gunCount--;
+                gun2.SetActive(false);
+                if(gunCount == 1)
+                {
+                    AK.SetActive(true);
+                }
+            }
+        }
+
+        if(gunCount == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))//Weapon Switch Mouse1 == Right Mouse Click
+            {
+                if (gun2.activeSelf)
+                {
+                    gun2.SetActive(false);
+                    AK.SetActive(true);
+                }
+                else
+                {
+                    if (AK.activeSelf)
+                    {
+                        AK.SetActive(false);
+                        gun2.SetActive(true);
+                    }
+                }
+            }
+        }//end if
+    }//end update
 }
