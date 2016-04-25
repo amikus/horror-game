cb6374b613a023b0257f58e0ef64f6a36cca46fc
ghfrench <gabefrenchy@gmail.com>
2016-04-24 20:29:58 -0400

    Reimport weapon assets with 2 new weapons and weapon improvements

--- SoftwareEngineering/SoftwareEngineeringProject/Assets/Weapons/WeaponActions.cs
+++ SoftwareEngineering/SoftwareEngineeringProject/Assets/Weapons/WeaponActions.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class WeaponActions : MonoBehaviour {
 
 	public Weapon weapon;
+	public GameObject[] weapons = new GameObject[3];
 
-	void Start () {}
+	void Start () {
+		weapons [0].SetActive (true);
+		weapons [1].SetActive (false);
+		weapons [2].SetActive (false);
+	}//Start
 
 	void Update () {
 		Vector3 forward = weapon.rayCaster.transform.TransformDirection (Vector3.forward) * weapon.range;
@@ -13,11 +18,30 @@ public class WeaponActions : MonoBehaviour {
 		RaycastHit hit;
 		Recoil ();
 
-		if (Input.GetKeyDown (KeyCode.R) && weapon.ammoReserve >= 1 && weapon.ammoInClip < weapon.clipSize) {
-			StartCoroutine(Reload ());
-		}
+		if (Input.GetKeyDown (KeyCode.C)) {
+			if (weapons [0].activeSelf == true) {
+				weapons [0].SetActive (false);
+				weapons [1].SetActive (true);
+			}
+			else if (weapons [1].activeSelf == true) {
+				weapons [1].SetActive (false);
+				weapons [2].SetActive (true);
+			}
+			else {
+				weapons [2].SetActive (false);
+				weapons [0].SetActive (true);
+			}
+		}//weapon switching
 
-		if (weapon.type == Weapon.weaponType.semi && Input.GetMouseButtonDown (0) && weapon.ammoInClip >= 1) {
+		if (Input.GetKeyDown (KeyCode.R) || Input.GetMouseButtonDown (0)) {
+			StopCoroutine ("Reload");
+		}//stop reloading
+
+		if (Input.GetKeyDown (KeyCode.R) && weapon.ammoReserve >= 1 && weapon.ammoInClip < weapon.clipSize) {
+			StartCoroutine ("Reload");
+		}//reloading
+
+		if (weapon.mode == Weapon.fireMode.semi && Input.GetMouseButtonDown (0) && weapon.ammoInClip >= 1) {
 			weapon.ammoInClip --;
 			weapon.muzzleFlash.Stop ();
 			weapon.fireSound.Stop ();
@@ -25,21 +49,21 @@ public class WeaponActions : MonoBehaviour {
 			weapon.fireSound.Play ();
 			if (Physics.Raycast (weapon.rayCaster.transform.position, forward, out hit, weapon.range)) {
 				if (hit.transform.tag == "Enemy") {
+					weapon.bloodSplatter.transform.position = hit.point;
+					weapon.bloodSplatter.Play ();
 					EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth> ();
 					if (enemyHealth != null) {
 						enemyHealth.TakeDamage (weapon.DPB);
 					}
-					weapon.bloodSplatter.transform.position = hit.point;
-					weapon.bloodSplatter.Play ();
 				}
 				else {
 					Instantiate (weapon.bullethole, hit.point, Quaternion.FromToRotation (Vector3.up, hit.normal));
 				}
 			}
 			weapon.recoil += Time.deltaTime/(60/weapon.RPM);
-		}
+		}//firing semi
 
-		if (weapon.type == Weapon.weaponType.auto && Input.GetMouseButton (0) && weapon.ammoInClip >= 1) {
+		if (weapon.mode == Weapon.fireMode.auto && Input.GetMouseButton (0) && weapon.ammoInClip >= 1) {
 			int aIC = (int)weapon.ammoInClip;
 			weapon.ammoInClip -= Time.deltaTime/(60/weapon.RPM);
 			if (aIC == (int)weapon.ammoInClip + 1) {
@@ -49,12 +73,12 @@ public class WeaponActions : MonoBehaviour {
 				weapon.fireSound.Play ();
 				if (Physics.Raycast (weapon.rayCaster.transform.position, forward, out hit, weapon.range)) {
 					if (hit.transform.tag == "Enemy") {
+						weapon.bloodSplatter.transform.position = hit.point;
+						weapon.bloodSplatter.Play ();
 						EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth> ();
 						if (enemyHealth != null) {
 							enemyHealth.TakeDamage (weapon.DPB);
 						}
-						weapon.bloodSplatter.transform.position = hit.point;
-						weapon.bloodSplatter.Play ();
 					}
 					else {
 						Instantiate (weapon.bullethole, hit.point, Quaternion.FromToRotation (Vector3.up, hit.normal));
@@ -62,7 +86,7 @@ public class WeaponActions : MonoBehaviour {
 				}
 				weapon.recoil += Time.deltaTime/(60/weapon.RPM);
 			}
-		}
+		}//firing auto
 	}//Update
 
 	void OnGUI() {
@@ -72,36 +96,44 @@ public class WeaponActions : MonoBehaviour {
 	}//OnGUI
 
 	IEnumerator Reload() {
-		weapon.reloadSound.Play ();
-		yield return new WaitForSeconds (weapon.reloadTime);
-		if (weapon.ammoReserve >= weapon.clipSize) {
-			weapon.ammoReserve -= (weapon.clipSize - weapon.ammoInClip);
-			weapon.ammoInClip = weapon.clipSize;
-		}
-		else if (weapon.ammoReserve + weapon.ammoInClip > weapon.clipSize) {
-			weapon.ammoReserve -= (weapon.clipSize - weapon.ammoInClip);
-			weapon.ammoInClip = weapon.clipSize;
+		if (weapon.type == Weapon.weaponType.shotgun) {
+			for (int i = 0; weapon.ammoInClip != weapon.clipSize; i++) {
+				weapon.reloadSound.Play ();
+				yield return new WaitForSeconds (weapon.reloadTime);
+				weapon.ammoReserve--;
+				weapon.ammoInClip++;
+			}
 		}
 		else {
-			weapon.ammoInClip += weapon.ammoReserve;
-			weapon.ammoReserve = 0;
+			weapon.reloadSound.Play ();
+			yield return new WaitForSeconds (weapon.reloadTime);
+			if (weapon.ammoReserve >= weapon.clipSize) {
+				weapon.ammoReserve -= (weapon.clipSize - weapon.ammoInClip);
+				weapon.ammoInClip = weapon.clipSize;
+			}
+			else if (weapon.ammoReserve + weapon.ammoInClip > weapon.clipSize) {
+				weapon.ammoReserve -= (weapon.clipSize - weapon.ammoInClip);
+				weapon.ammoInClip = weapon.clipSize;
+			}
+			else {
+				weapon.ammoInClip += weapon.ammoReserve;
+				weapon.ammoReserve = 0;
+			}
 		}
-		weapon.reloadSound.Stop ();
 	}//Reload
 
 	void Recoil() {
 		if (weapon.recoil > 0) {
 			var maxRecoil = Quaternion.Euler(weapon.maxRecoilX+Random.Range( -5, 5), weapon.maxRecoilY+Random.Range(-5, 5), 0);
-			weapon.model.transform.localRotation = Quaternion.Slerp(weapon.model.transform.localRotation, maxRecoil, Time.deltaTime*weapon.recoilRate);
-			weapon.model.transform.localEulerAngles = new Vector3(weapon.model.transform.localEulerAngles.x, weapon.model.transform.localEulerAngles.y, weapon.model.transform.localEulerAngles.z);
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, maxRecoil, Time.deltaTime*weapon.recoilRate);
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
 			weapon.recoil -= Time.deltaTime/(60/weapon.RPM);
 		}
 		else
 		{
 			weapon.recoil = 0;
-			var minRecoil = Quaternion.Euler(0, 0, 0);
-			weapon.model.transform.localRotation = Quaternion.Slerp(weapon.model.transform.localRotation, Quaternion.identity, Time.deltaTime*weapon.recoilRate);
-			weapon.model.transform.localEulerAngles = new Vector3(weapon.model.transform.localEulerAngles.x, weapon.model.transform.localEulerAngles.y, weapon.model.transform.localEulerAngles.z);
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.identity, Time.deltaTime*weapon.recoilRate);
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
 		}
 	}//Recoil
 
@@ -110,17 +142,16 @@ public class WeaponActions : MonoBehaviour {
 [System.Serializable]
 public class Weapon {
 	public string name;
-	public GameObject model;
 	public AudioSource fireSound;
 	public AudioSource reloadSound;
 	public ParticleSystem muzzleFlash;
 	public ParticleSystem bloodSplatter;
 	public GameObject rayCaster;
 	public GameObject bullethole;
-	public enum weaponAmmo {pistol = 0, shotgun = 1, autorifle = 2};
-	public weaponAmmo ammo;
-	public enum weaponType {semi = 0, auto = 1};
+	public enum weaponType {pistol = 0, shotgun = 1, autorifle = 2};
 	public weaponType type;
+	public enum fireMode {semi = 0, auto = 1};
+	public fireMode mode;
 	public int DPB = 0;
 	public float RPM = 0;
 	public float clipSize = 0;
